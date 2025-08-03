900eebaba8865f13f1d16b7a50bbc2b2713c79a4
Mr_Thuan <nguyenvonhatthuan@gmail.com>
2025-08-03 14:02:49 +0700

    change

--- TD/Resource_Management.cpp
+++ TD/Resource_Management.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Resource_Management.h"
 #include <iostream>
 #include <stdexcept>
@@ -159,6 +158,3 @@ const std::vector<sf::Texture>& Resource_Management::getTexture(const std::strin
     // Nếu không tìm thấy, ném một ngoại lệ rõ ràng
     throw std::runtime_error("Texture not found: " + name);
 }
-=======
-
->>>>>>> e70c06d351099523de1e2b69bba52599da428017
